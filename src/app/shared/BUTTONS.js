f76aed696bd206d3e0ca11ff17f9ad8876d7fb7a
Code <dmkcodes@gmail.com>
2023-01-21 17:54:38 -0500

    list and card features, filter component

---
+++ src/app/shared/BUTTONS.js
@@ -0,0 +1,129 @@
+import Bug from '../assets/types/bug.png';
+import Dark from '../assets/types/dark.png';
+import Dragon from '../assets/types/dragon.png';
+import Electric from '../assets/types/electric.png';
+import Fairy from '../assets/types/fairy.png';
+import Fighting from '../assets/types/fighting.png';
+import Fire from '../assets/types/fire.png';
+import Flying from '../assets/types/flying.png';
+import Ghost from '../assets/types/ghost.png';
+import Grass from '../assets/types/grass.png';
+import Ground from '../assets/types/ground.png';
+import Ice from '../assets/types/ice.png';
+import Normal from '../assets/types/normal.png';
+import Poison from '../assets/types/poison.png';
+import Psychic from '../assets/types/psychic.png';
+import Rock from '../assets/types/rock.png';
+import Steel from '../assets/types/steel.png';
+import Water from '../assets/types/water.png';
+
+export const buttons = [
+    {
+        id: 1,
+        name: 'Bug',
+        value: 'bug',
+        image: Bug
+    },
+    {
+        id: 2,
+        name: 'Dark',
+        value: 'dark',
+        image: Dark
+    },
+    {
+        id: 3,
+        name: 'Dragon',
+        value: 'dragon',
+        image: Dragon
+    },
+    {
+        id: 4,
+        name: 'Electric',
+        value: 'electric',
+        image: Electric
+    },
+    {
+        id: 5,
+        name: 'Fairy',
+        value: 'fairy',
+        image: Fairy
+    },
+    {
+        id: 6,
+        name: 'Fighting',
+        value: 'fighting',
+        image: Fighting
+    },
+    {
+        id: 7,
+        name: 'Fire',
+        value: 'fire',
+        image: Fire
+    },
+    {
+        id: 8,
+        name: 'Flying',
+        value: 'flying',
+        image: Flying
+    },
+    {
+        id: 9,
+        name: 'Ghost',
+        value: 'ghost',
+        image: Ghost
+    },
+    {
+        id: 10,
+        name: 'Grass',
+        value: 'grass',
+        image: Grass
+    },
+    {
+        id: 11,
+        name: 'Ground',
+        value: 'ground',
+        image: Ground
+    },
+    {
+        id: 12,
+        name: 'Ice',
+        value: 'ice',
+        image: Ice
+    },
+    {
+        id: 13,
+        name: 'Normal',
+        value: 'normal',
+        image: Normal
+    },
+    {
+        id: 14,
+        name: 'Poison',
+        value: 'poison',
+        image: Poison
+    },
+    {
+        id: 15,
+        name: 'Psychic',
+        value: 'psychic',
+        image: Psychic
+    },
+    {
+        id: 16,
+        name: 'Rock',
+        value: 'rock',
+        image: Rock
+    },
+    {
+        id: 17,
+        name: 'Steel',
+        value: 'steel',
+        image: Steel
+    },
+    {
+        id: 18,
+        name: 'Water',
+        value: 'water',
+        image: Water
+    }
+];
